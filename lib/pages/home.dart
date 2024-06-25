1518a7e91f713483daa9609a5fdf16c72f81332a
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-24 20:14:56 -0600

    Add new SVG icons for dots, arrow, search, button, and filter

---
+++ lib/pages/home.dart
@@ -0,0 +1,105 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: appBar(),
+      body: _searchField(),
+    );
+  }
+
+  Column _searchField() {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 40,
+                spreadRadius: 0.0,
+              )
+            ],
+          ),
+          child: const TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: "Search for recipes",
+              hintStyle: TextStyle(
+                color: Colors.grey,
+              ),
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(12),
+                child: Icon(
+                  Icons.search,
+                ),
+              ),
+              suffixIcon: SizedBox(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Icon(
+                          Icons.filter_list,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text("Breakfast",
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold)),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: const Icon(
+          Icons.arrow_back,
+          color: Colors.black,
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: const Icon(
+            Icons.more_horiz,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+}
